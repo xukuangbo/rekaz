ae72e144e227ffcf97c024252f8a999a902eef2b
GLChan <cglcgl000@gmail.com>
2016-02-19 21:24:32 +0800

    first commit
    first commit

---
+++ rekaz/Zaker-C/Zaker-C/Classes/Community(社区)/Controller/ZKRCommunityViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZKRCommunityViewController.h
+//  Zaker-C
+//
+//  Created by ShaoXinSen on 16/1/25.
+//  Copyright © 2016年 SKX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKRCommunityViewController : UIViewController
+
+@end
