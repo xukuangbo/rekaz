7f8fa1923fd3a02c02a591a4f8c606df199eba9d
GLChan <cglcgl000@gmail.com>
2016-04-06 17:59:05 +0800

    udpate 4.6
    修复玩乐界面返回时导航栏消失的bug

--- rekaz/Zaker-C/Zaker-C/Classes/Fun(玩乐)/Controller/ZKRFunViewController.m
+++ rekaz/Zaker-C/Zaker-C/Classes/Fun(玩乐)/Controller/ZKRFunViewController.m
@@ -67,7 +67,15 @@
     return _tableVC;
 }
 
+
+
 #pragma mark - ---| init |---
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
